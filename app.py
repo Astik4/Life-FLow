9c65f84948aaf0cb562745211c9b23443ee472c3
Astik4 <guptaastik695@gmail.com>
2026-03-31 02:13:27 +0530

    updated app.py

--- app.py
+++ app.py
@@ -159,11 +159,9 @@ def all_donors():
 
 @app.route("/search_donor/<donor_id>", methods=["GET"])
 def search_donor(donor_id):
-    blood_group = request.args.get("blood_group")
-    city = request.args.get("city")
     try:
         cur = safe_cursor()
-        cur.execute("SELECT * FROM donor WHERE Donor_id = %s AND City = %s", (donor_id, city))
+        cur.execute("SELECT * FROM donor WHERE Donor_id and = %s", (donor_id,))
         row = cur.fetchone()
         if not row:
             return error("Donor not found.", 404)
